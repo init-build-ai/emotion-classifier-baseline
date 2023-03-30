69104cf635d6839c39c09dc64cf7dce6d9039c20
Jacob Schuster <jschuster8765@gmail.com>
2023-03-30 17:15:47 -0400

    fix cors

--- Inference.py
+++ Inference.py
@@ -7,24 +7,23 @@ from PIL import Image
 import onnxruntime
 import numpy as np
 
-
 class Inference:
     def __init__(self):
         self.transform = transforms.Compose([transforms.ToTensor(), transforms.Resize(size=(224, 224))])
         self.ort_session = onnxruntime.InferenceSession("emotion_3080.onnx")
 
-    def forward(self, img):
-        im_bytes = base64.b64decode(img)   # im_bytes is a binary image
-        im_file = BytesIO(im_bytes)  # convert image to file-like object
-        img = Image.open(im_file)   # img is now PIL Image object
-
+    def forward(self, image):
+        image_data = base64.b64decode(image.split(",")[1])
+        img = Image.open(BytesIO(image_data))
+        
         img_transform = np.array(self.transform(img))
         img_transform = img_transform.reshape(1,3,224,224)
+        
         ort_inputs = {self.ort_session.get_inputs()[0].name: np.array(img_transform)}
         ort_outs = self.ort_session.run(None, ort_inputs)
+        
         sm = torch.tensor(ort_outs)
         sm = sm.reshape(7,-1)
-        #print("output", sm, sm.shape)
+        
         ort_outs = F.softmax(sm, dim=0)
-        #print("RESULTS", ort_outs)
         return ort_outs
